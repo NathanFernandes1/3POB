1e0d573e3fe1041f0ad73ef75f6fcca5cd8dc7d1
Nathan <103209917+NathanFernandes1@users.noreply.github.com>
2024-04-04 11:22:29 -0300

    Create Main.java

---
+++ Disciplina com constructor/Main.java
@@ -0,0 +1,32 @@
+package br.com.escola20241;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       
+        
+        Disciplina disciplina = new Disciplina("Projeto de banco de dados", "3PBD", "120","3");
+        
+        System.out.println("Nome: " + disciplina.getNome());
+        System.out.println("Sigla: " + disciplina.getSigla());
+        System.out.println("Carga Horaria: " + disciplina.getCargaHoraria());
+        System.out.println("Semestre: " + disciplina.getSemestre());
+        
+        /*String nome = "";
+        String sigla = "";
+        String cargaHoraria = "";
+        String semestre = "";*/
+        
+        /*Disciplina disciplina = new Disciplina();
+        
+        disciplina.setNome("Projeto de banco de dados");
+        disciplina.setSigla("PBD");
+        disciplina.setCargaHoraria("120");
+        disciplina.setSemestre("3");*/
+        
+        
+    }
+
+}
