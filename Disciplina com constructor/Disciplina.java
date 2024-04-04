15e45e6a827ac14576e72d6f9a9b95a3814e20e3
Nathan <103209917+NathanFernandes1@users.noreply.github.com>
2024-04-04 11:23:51 -0300

    Create Disciplina.java

---
+++ Disciplina com constructor/Disciplina.java
@@ -0,0 +1,43 @@
+package br.com.escola20241;
+
+public class Disciplina {
+	private String nome;
+	private String sigla;
+	private String cargaHoraria;
+	private String semestre;
+	
+	
+	public Disciplina(String nome, String sigla, String cargaHoraria, String semestre) {
+		super();
+		this.nome = nome;
+		this.sigla = sigla;
+		this.cargaHoraria = cargaHoraria;
+		this.semestre = semestre;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	public String getCargaHoraria() {
+		return cargaHoraria;
+	}
+	public void setCargaHoraria(String cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+	public String getSemestre() {
+		return semestre;
+	}
+	public void setSemestre(String semestre) {
+		this.semestre = semestre;
+	}
+}
